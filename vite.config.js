34d33bc3d2ecb6c7ac295246d33b0e84f55fd094
Mejrab <abdelmoinfly@gmai.com>
2025-07-28 16:46:48 +0200

    DIST config

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,15 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
-import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
+    build: {
+        outDir: 'dist', // <-- this is what Vercel expects
+        emptyOutDir: true
+    },
     plugins: [
         laravel({
             input: ['resources/css/app.css', 'resources/js/app.js'],
             refresh: true,
         }),
-        tailwindcss(),
     ],
 });
